a845630ebf1c96435981899b11694977db38fd75
Eric S <eric@laurila.se>
2021-06-02 19:34:43 +0200

    adds iteration method

--- FibonacciNumbers/FibonacciNumbers.cpp
+++ FibonacciNumbers/FibonacciNumbers.cpp
@@ -9,10 +9,29 @@ int Recursion(int n){
     return Recursion(n - 1) + Recursion(n - 2);
 }
 
-int main() {
+int iteration(int n) {
+    int current = 0, previous = 1, next = 0;
+    for (int i = 0; i < n; i++) {
+        cout << current << endl;
+        next = current + previous;
+        current = previous;
+        previous = next;
+    }
+    return 0;
+}
+
+/*
+void main() {
     int input;
     cout << "Welcome, Please write a number to get the Fibonacci number" << endl;
     cin >> input;
     cout << Recursion(input);
-    return 0;
+}
+*/
+
+int main() {
+    int input;
+    cout << "Welcome, Please write a number to iterate to that Fibonacci number." << endl;
+    cin >> input;
+    cout << iteration(input);
 }
