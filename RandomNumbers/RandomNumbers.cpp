918fcb4cf4a184a9dcda87105811b85dfbf8138a
Eric S <eric@laurila.se>
2021-06-04 12:43:14 +0200

    Implements RandomNumbers

--- RandomNumbers/RandomNumbers.cpp
+++ RandomNumbers/RandomNumbers.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "Hello World!\n";
-}
+    int NumberPool = 10000;
+    int RandomNumbers[21] = { 0 };
 
+    for (int i = 0; i <= NumberPool; i++) {
+        RandomNumbers[rand() % 21]++;
+    }
+
+    for (int i = 1; i <= 20; i++) {
+        float Percentage = (float)RandomNumbers[i] * 100 / NumberPool;
+        cout << "Number " << i << ": " << RandomNumbers[i] << " (" << Percentage << "%)\n";
+    }
+}
